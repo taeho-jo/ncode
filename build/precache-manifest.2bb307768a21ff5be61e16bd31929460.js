16c898a3ed66adfbd044fc454bcd486bef5aca04
jotang <jotang3726@gmail.com>
2020-03-04 17:19:04 +0900

    dcode

---
+++ build/precache-manifest.2bb307768a21ff5be61e16bd31929460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a4fcb59dd46aea3ca763fc8cb80430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf7f40a4b1935760878",
+    "url": "/static/js/2.4e79fea1.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/static/js/2.4e79fea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2991e3924b6ebf015f54",
+    "url": "/static/js/main.c665446f.chunk.js"
+  },
+  {
+    "revision": "3e80e5d3c1ab3e18a3e2",
+    "url": "/static/js/runtime-main.09d710e3.js"
+  }
+]);
